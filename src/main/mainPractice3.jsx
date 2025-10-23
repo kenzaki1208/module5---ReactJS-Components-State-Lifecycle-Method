072087c8ee3fcc1eb3f034ed4ef004c4850c7010
kenzaki1208 <duytrinh25072001@gmail.com>
2025-10-23 09:48:15 +0700

    update project

---
+++ src/main/mainPractice3.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppPractice3 from "../app/AppPractice3";
+
+const root = ReactDOM.createRoot(document.getElementById("root3"));
+root.render(<AppPractice3 />)
