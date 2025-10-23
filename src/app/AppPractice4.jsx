888b601b8efaf637086b2d40ba01900d58f14133
kenzaki1208 <duytrinh25072001@gmail.com>
2025-10-23 10:03:33 +0700

    update project

---
+++ src/app/AppPractice4.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function AppPractice4() {
+    const [color, setColor] = useState("black");
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setColor("pink");
+        }, 3000);
+        
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div>
+            <div style={{backgroundColor: color, paddingTop: 20, width: 400, height: 80, margin: "auto"}} />
+        </div>
+    );
+}
+
+export default AppPractice4;
---
+++ src/main/mainPractice4.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import AppPractice4 from '../app/AppPractice4'
+
+createRoot(document.getElementById('root4')).render(
+    <StrictMode>
+        <AppPractice4 />
+    </StrictMode>,
+)
