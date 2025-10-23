b32c8037c2f7c6008e5b7643cd6118f37014cebd
kenzaki1208 <duytrinh25072001@gmail.com>
2025-10-23 09:40:19 +0700

    update project

---
+++ src/app/AppPractice2.jsx
@@ -0,0 +1,11 @@
+import AddComponent2 from "../components/AddConponent2";
+
+function AppPractice2() {
+    return (
+        <div className="App">
+            <AddComponent2 firstNumber={1} secondNumber={2} />
+        </div>
+    );
+}
+
+export default AppPractice2;
