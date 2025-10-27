5a69c94051b4857d634f99cefcc763958b803e78
kenzaki1208 <duytrinh25072001@gmail.com>
2025-10-27 09:43:46 +0700

    update project

---
+++ src/components/HomeForm.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class HomeForm extends Component {
+    componentWillUnmount() {
+        alert("Goodbye!");
+    }
+
+    render() {
+        return (
+            <div className="container d-flex align-items-center text-center">
+                <div className="form-signin">
+                    <h1 className="h3 mb-3 fw-normal">Welcome</h1>
+                    <button className="w-100 btn btn-lg btn-primary" type="button" onClick={this.props.onLogOut}>
+                        Log Out
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default HomeForm;
---
+++ src/main/mainExercise5.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import AppExercise5 from '../app/AppExercise5'
+// import './index.css'
+
+createRoot(document.getElementById('rootE5')).render(
+  <StrictMode>
+    <AppExercise5 />
+  </StrictMode>,
+)
